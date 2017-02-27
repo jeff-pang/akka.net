2481b9aab71018195220e3e95afda023c76d0867
ravengerUA <yurisikorsky@gmail.com>
2017-02-27 17:45:18 +0200

    Converted Akka project

--- src/core/Akka/IO/SocketChannel.cs
+++ src/core/Akka/IO/SocketChannel.cs
@@ -6,8 +6,10 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using Akka.Actor;
 using Akka.Util;
 
@@ -96,6 +98,31 @@ namespace Akka.IO
         /// <returns>TBD</returns>
         public bool Connect(EndPoint address)
         {
+#if CORECLR
+            AsyncCallback endAction = ar => { };
+            var beginConnectMethod = _socket
+                .GetType()
+                .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(method => method.Name.Equals("BeginConnect"))
+                .FirstOrDefault(method =>
+                {
+                    var parameters = method.GetParameters();
+                    return parameters.Length == 3 && parameters[0].ParameterType == typeof(EndPoint) &&
+                           parameters[1].ParameterType == typeof(AsyncCallback) && parameters[2].ParameterType == typeof(object);
+                });
+
+            _connectResult = (IAsyncResult)beginConnectMethod.Invoke(_socket, new object[] { address, endAction, null });
+            if (_connectResult.CompletedSynchronously)
+            {
+                _socket
+                    .GetType()
+                    .GetMethod("EndConnect", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Invoke(_socket, new object[] { _connectResult });
+                _connected = true;
+                return true;
+            }
+            return false;
+#else
             _connectResult = _socket.BeginConnect(address, ar => { }, null);
             if (_connectResult.CompletedSynchronously)
             {
@@ -104,8 +131,8 @@ namespace Akka.IO
                 return true;
             }
             return false;
+#endif
         }
-
 
         /// <summary>
         /// TBD
@@ -113,6 +140,19 @@ namespace Akka.IO
         /// <returns>TBD</returns>
         public bool FinishConnect()
         {
+#if CORECLR
+            if (_connectResult.CompletedSynchronously)
+                return true;
+            if (_connectResult.IsCompleted)
+            {
+                _socket
+                    .GetType()
+                    .GetMethod("EndConnect", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Invoke(_socket, new object[] { _connectResult });
+                _connected = true;
+            }
+            return _connected;
+#else
             if (_connectResult.CompletedSynchronously)
                 return true;
             if (_connectResult.IsCompleted)
@@ -120,7 +160,9 @@ namespace Akka.IO
                 _socket.EndConnect(_connectResult);
                 _connected = true;
             }
+
             return _connected;
+#endif
         }
 
         /// <summary>
